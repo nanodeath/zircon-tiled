9cb086e80116f245616c62747f278a251e09698f
Max A <nanodeath@gmail.com>
2021-03-20 14:03:48 -0700

    Optimize Java2DTiledTileset.kt.
    Closes #10.

--- src/main/kotlin/org/hexworks/zirconx/api/tiled/Java2DTiledTileset.kt
+++ src/main/kotlin/org/hexworks/zirconx/api/tiled/Java2DTiledTileset.kt
@@ -1,46 +1,44 @@
 package org.hexworks.zirconx.api.tiled
 
-import com.github.benmanes.caffeine.cache.Caffeine
 import org.hexworks.zircon.api.data.GraphicalTile
 import org.hexworks.zircon.api.data.Position
 import org.hexworks.zircon.api.data.Tile
 import org.hexworks.zirconx.api.tiled.ext.TiledTilesetFile
 import org.hexworks.zirconx.internal.tiled.logDuration
+import org.hexworks.zirconx.internal.tiled.logDurationNs
 import java.awt.Graphics2D
 import java.awt.image.BufferedImage
 import java.io.File
 import java.io.InputStream
 import javax.imageio.ImageIO
 
-class Java2DTiledTileset(
+class Java2DTiledTileset internal constructor(
     image: () -> InputStream,
     private val indexOffset: Int,
     private val tileWidth: Int,
     private val tileHeight: Int,
-    private val columns: Int,
+    columns: Int,
+    tileCount: Int,
     name: String
 ) {
     private val image = logDuration("Java2DTiledTileset.image($name)") { ImageIO.read(image()) }
-    private val textureCache = logDuration("Java2DTiledTileset.Caffeine") {
-        Caffeine.newBuilder()
-            .maximumSize(1024L)
-            .build<Int, BufferedImage>()
-    }
+
+    private val subimageLoader: SubimageLoader = SimpleArrayCache(
+        tileCount,
+        RealSubimageLoader(this.image, tileWidth, tileHeight, columns)
+    )
 
     fun drawTile(tile: Tile, surface: Graphics2D, position: Position) {
         val idx = (tile as GraphicalTile).name.toInt() - indexOffset
-        val texture = textureCache.get(idx, this::loadTexture)
+        val texture = logDurationNs("loadTexture", 300_000) { subimageLoader.loadSubimage(idx) }
 
         val x = position.x * tileWidth
         val y = position.y * tileHeight
         surface.drawImage(texture, x, y, null)
     }
 
-    private fun loadTexture(tileIdx: Int): BufferedImage =
-        image.getSubimage(tileIdx % columns * tileWidth, tileIdx / columns * tileHeight, tileWidth, tileHeight)
-
     companion object {
-        fun from(data: TiledTilesetFile, offset: Int, tileMapFile: File): Java2DTiledTileset {
+        internal fun from(data: TiledTilesetFile, offset: Int, tileMapFile: File): Java2DTiledTileset {
             val imageFile = tileMapFile.parentFile.resolve(data.image.source)
             val image = { imageFile.inputStream() }
             return logDuration("Java2DTiledTileset.constructor(${imageFile.name})") {
@@ -50,9 +48,40 @@ class Java2DTiledTileset(
                     data.tilewidth,
                     data.tileheight,
                     data.columns,
+                    data.tilecount,
                     imageFile.name
                 )
             }
         }
     }
+}
+
+private interface SubimageLoader {
+    fun loadSubimage(idx: Int): BufferedImage
+}
+
+/**
+ * Image loader that actually accesses the provided [BufferedImage]. Slow.
+ */
+private class RealSubimageLoader(
+    private val image: BufferedImage,
+    private val tileWidth: Int,
+    private val tileHeight: Int,
+    private val columns: Int
+) : SubimageLoader {
+    override fun loadSubimage(idx: Int): BufferedImage =
+        image.getSubimage(idx % columns * tileWidth, idx / columns * tileHeight, tileWidth, tileHeight)
+}
+
+/**
+ * Basic array-based cache that can store a fixed number of textures. Not threadsafe.
+ */
+private class SimpleArrayCache(maxSize: Int, private val delegate: SubimageLoader) : SubimageLoader {
+    private val cache: Array<BufferedImage?> = arrayOfNulls(maxSize)
+
+    override fun loadSubimage(idx: Int): BufferedImage =
+        when (val cachedValue = cache[idx]) {
+            null -> delegate.loadSubimage(idx).also { cache[idx] = it }
+            else -> cachedValue
+        }
 }
--- src/main/kotlin/org/hexworks/zirconx/internal/tiled/Performance.kt
+++ src/main/kotlin/org/hexworks/zirconx/internal/tiled/Performance.kt
@@ -1,5 +1,7 @@
 package org.hexworks.zirconx.internal.tiled
 
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
@@ -8,6 +10,15 @@ import kotlin.contracts.contract
 private val checkingPerformance = System.getenv("MEASURE_PERFORMANCE").toBoolean()
 
 private val nesting = AtomicInteger(0)
+
+internal data class PerfData(var counter: Int, var runningTotal: Long) {
+    fun reset() {
+        counter = 0
+        runningTotal = 0
+    }
+}
+
+private val counters: ConcurrentMap<String, PerfData> = ConcurrentHashMap()
 
 @OptIn(ExperimentalContracts::class)
 internal inline fun <T> logDuration(name: String, callback: () -> T): T {
@@ -24,6 +35,37 @@ internal inline fun <T> logDuration(name: String, callback: () -> T): T {
     val durationMs = System.currentTimeMillis() - start
     val slow = if (durationMs >= 100) "\uD83D\uDD25" else "" // 🔥
     System.err.println("PERF: ${generateNesting(currentNesting)}$slow$name took ${durationMs}ms")
+    return result
+}
+
+@OptIn(ExperimentalContracts::class)
+internal inline fun <T> logDurationNs(name: String, logEvery: Int = 0, callback: () -> T): T {
+    contract {
+        callsInPlace(callback, InvocationKind.EXACTLY_ONCE)
+    }
+    if (!checkingPerformance) {
+        return callback()
+    }
+    val start = System.nanoTime()
+    val currentNesting = nesting.getAndIncrement()
+    val result = callback()
+    nesting.decrementAndGet()
+    val durationNs = System.nanoTime() - start
+    if (logEvery <= 0) {
+        System.err.println("PERF: ${generateNesting(currentNesting)}$name took ${durationNs}ns")
+    } else {
+        val myData = counters.computeIfAbsent(name) { PerfData(0, 0L) }
+        synchronized(myData) {
+            myData.runningTotal += durationNs
+            if (++myData.counter == logEvery) {
+                val invocations = myData.counter
+                val totalTime = myData.runningTotal
+                myData.reset()
+                val formattedTime = (totalTime.toDouble() / invocations).toInt()
+                System.err.println("PERF: ${generateNesting(currentNesting)}$name took ${formattedTime}ns on average ($invocations hits)")
+            }
+        }
+    }
     return result
 }
 
